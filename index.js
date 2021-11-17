9f03fe557bfd5d53fdaeaae61f535b229ef2bdda
David Silveira <daviewave@gmail.com>
2021-11-17 16:00:06 -0500

    upon program init, the user is prompted with the questions from the questions array needed to create the readme file

--- index.js
+++ index.js
@@ -7,47 +7,51 @@ const questions = [
   {
     type: "input",
     name: "projectTitle",
-    info: "Enter the title of the project you would like to generate a README file for: ",
+    message:
+      "Enter the title of the project you would like to generate a README file for: ",
   },
   {
     type: "input",
     name: "projectDescription",
-    info: "Enter a description of the project you would like to add to the README file: ",
+    message:
+      "Enter a description of the project you would like to add to the README file: ",
   },
   {
     type: "input",
     name: "projectInstallation",
-    info: "Enter the way in which the project was installed: ",
+    message: "Enter the way in which the project was installed: ",
   },
   {
     type: "input",
     name: "projectUsage",
-    info: "Enter how a user will use the project: ",
+    message: "Enter how a user will use the project: ",
   },
   {
     type: "input",
     name: "projectLicense",
-    info: "Enter the license this project used: ",
+    message: "Enter the license this project used: ",
   },
   {
     type: "input",
     name: "projectContribution",
-    info: "Enter they ways in which users could contribute to the project: ",
+    message: "Enter they ways in which users could contribute to the project: ",
   },
   {
     type: "input",
     name: "projectTesting",
-    info: "Enter directions that will allow the user to test the project: ",
+    message: "Enter directions that will allow the user to test the project: ",
   },
   {
     type: "input",
     name: "projectEmail",
-    info: "Enter the email address associated with the creator of the project: ",
+    message:
+      "Enter the email address associated with the creator of the project: ",
   },
   {
     type: "input",
     name: "projectGithub",
-    info: "Enter the Github account associated with the creator of the project: ",
+    message:
+      "Enter the Github account associated with the creator of the project: ",
   },
 ];
 console.log(questions);
@@ -56,7 +60,9 @@ console.log(questions);
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions);
+}
 
 // Function call to initialize app
 init();
